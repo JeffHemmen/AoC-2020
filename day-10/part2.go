093bd9de36cee64d3d58350a78355b919a886d0d
Jeff Hemmen <jeff.hemmen@ecs-digital.co.uk>
2020-12-18 19:55:37 +0100

    WIP: Day 10 Part 2 (undo math.big stuff)

--- day-10/part2.go
+++ day-10/part2.go
@@ -6,30 +6,28 @@ import (
 	"strconv"
 	"fmt"
 	"sort"
-	"math/big"
 )
 
 var adapters []int
 
-var dynamicProgramming map[int]big.Int
+var dynamicProgramming map[int]uint64
 
-func findCombinations(idx int) big.Int {
-	cache := dynamicProgramming[idx]
-	if cache.Cmp(new(big.Int)) != 0 {
+func findCombinations(idx int) uint64 {
+	if dynamicProgramming[idx] != 0 {
 		fmt.Println("dynamicProgramming[", idx, "] = ", dynamicProgramming[idx])
 		return dynamicProgramming[idx]
 	}
 	fmt.Println("current index: ", idx)
-	if idx == 0 { return *new(big.Int).SetInt64(int64(1)) }
+	if idx == 0 { return 1 }
 
 	// adapters[idx-1] always valid
-	ownFactor := 1
+	ownFactor := uint64(1)
 	recursiveFactor := findCombinations(idx-1)
 
 	// adapters[idx-2] conditional
 	if idx >= 2 && adapters[idx-2] >= adapters[idx] - 3 {
 		ownFactor++
-		recursiveFactor.Mul(&recursiveFactor, findCombinations(idx-2))
+		recursiveFactor *= findCombinations(idx-2)
 	}
 
 	// adapters[idx-3] conditional
@@ -37,7 +35,7 @@ func findCombinations(idx int) big.Int {
 		ownFactor++
 		recursiveFactor *= findCombinations(idx-3)
 	}
-	res := big.Mul(ownFactor, recursiveFactor)
+	res := ownFactor * recursiveFactor
 	fmt.Println("Completed index: ", idx, " (", adapters[idx], "); Result: ", res)
 	dynamicProgramming[idx] = res
 	return res
@@ -55,6 +53,6 @@ func main() {
 		adapters = append(adapters, this)
 	}
 	sort.Ints(adapters)
-	dynamicProgramming = make(map[int]big.Int)
+	dynamicProgramming = make(map[int]uint64)
 	fmt.Println(findCombinations(20))
 }
